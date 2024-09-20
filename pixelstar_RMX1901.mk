2035edb48acf006c074a5f8874d4325ba90c7da4
israfilbd <israfilbd0235@gmail.com>
2024-09-20 06:21:22 +0000

    Revert "RMX1921: Add ViPER4Android FX support"
    This reverts commit eb4c348a5632e6717db1a3ae58da20aaeffba9d4.

--- pixelstar_RMX1901.mk
+++ pixelstar_RMX1901.mk
@@ -14,9 +14,6 @@ $(call inherit-product, vendor/pixelstar/config/common_full_phone.mk)
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Viper
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
 # Sign builds
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/aosp-sign/keys/releasekey
 
