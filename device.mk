eed697b7949057d74bfccb3e73b80a9f9e1444f6
ij-israfil <israfilkhan494@gmail.com>
2024-10-29 17:32:39 +0600

    RMX1901: Drop dexpreopt speed apps
    *Fix systemui pinner service
    Signed-off-by: ij-israfil <israfilkhan494gmail.com>

--- device.mk
+++ device.mk
@@ -158,11 +158,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
-# Dexpreopt
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SettingsGoogle \
-    SystemUIGoogle
-
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
