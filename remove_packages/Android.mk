c5d9f9be1ebcb5c1d4ac4479a83c5adc53e724b3
ij-israfil <israfilkhan494@gmail.com>
2024-12-24 22:10:00 +0600

    RMX1901: Drop more gms packages
    Signed-off-by: Ij-Israfil <israfilkhan494gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,11 +5,11 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_OVERRIDES_PACKAGES := AudioFX PicoTts FMRadio Eleven Gallery2 Recorder
+LOCAL_OVERRIDES_PACKAGES := AudioFX PicoTts FMRadio FM2 Eleven Gallery2 Recorder
 LOCAL_OVERRIDES_PACKAGES += Glimpse PhotoTable AndroidAutoStubPrebuilt
 LOCAL_OVERRIDES_PACKAGES += GoogleTTS WellbeingPrebuilt FilesPrebuilt DreamlinerUpdater
 LOCAL_OVERRIDES_PACKAGES += TagGoogle talkback OdadPrebuilt MaestroPrebuilt
-LOCAL_OVERRIDES_PACKAGES += bcr CreativeAssistant KidsSupervisionStub
+LOCAL_OVERRIDES_PACKAGES += bcr KidsSupervisionStub MlkitBarcodeUIPrebuilt VisionBarcodePrebuilt
 LOCAL_OVERRIDES_PACKAGES += DreamlinerDreamsPrebuilt_100894 DreamlinerPrebuilt_22000020
 
 LOCAL_UNINSTALLABLE_MODULE := true
