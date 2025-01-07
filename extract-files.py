4f0cb2d4e2e8668ee6338d06a8f52d5ca12a34e1
Gérard Parat <github@le-cycliste.fr>
2025-01-07 22:53:02 +0100

    RMX1901: Switch to python extract-utils
    Change-Id: Ifad8b3065537a80acef5f391976374f6b582b430
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .add_needed('libcamera_metadata_shim.so'),
+    ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'RMX1901',
+    'realme',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
