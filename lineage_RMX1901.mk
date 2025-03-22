4811b1cbfe97aaa93b386e47ab81130d5b47d489
Ij-Israfil <israfilkhan494@gmail.com>
2025-03-22 12:42:11 +0600

    RMX1901: Update Matrix flags
    Signed-off-by: Ij-Israfil <israfilkhan494gmail.com>

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -7,6 +7,7 @@
 # Inherit some common Matrixx OS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_GSUITE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Matrixx Flags
