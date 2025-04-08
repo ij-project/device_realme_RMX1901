51086483f069ddd35aca6acde17bec38fdfcf4d8
Ij Israfil <israfilkhan494@gmail.com>
2025-04-08 14:46:04 +0000

    RMX1901: Set TARGET_DISABLE_EPPE := true
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -9,6 +9,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_MINI_GAPPS := true
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from RMX1901 device
