b944e9317ac6a4a16c3e73ce99e6dd2c43aa9ada
Ij-Israfil <israfilkhan494@gmail.com>
2025-08-16 19:16:09 +0600

    RMX1901: Tune zram for all variant
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -32,7 +32,8 @@ function configure_zram_parameters() {
     MemTotal=${MemTotalStr:16:8}
 
     echo lz4 > /sys/block/zram0/comp_algorithm
-    echo 100 > /proc/sys/vm/swappiness
+    echo 160 > /proc/sys/vm/swappiness
+    echo 60 > /proc/sys/vm/direct_swappiness
     echo 0 > /proc/sys/vm/page-cluster
 
     if [ -f /sys/block/zram0/disksize ]; then
@@ -41,14 +42,14 @@ function configure_zram_parameters() {
         fi
 
         if [ $MemTotal -le 4194304 ]; then
-            #config 3.5GB zram size with memory 4 GB
-            echo 3758096384 > /sys/block/zram0/disksize
+            #config 2.5GB zram size with memory 4 GB
+            echo 2684354560 > /sys/block/zram0/disksize
         elif [ $MemTotal -le 6291456 ]; then
-            #config 3.5GB zram size with memory 6 GB
-            echo 3758096384 > /sys/block/zram0/disksize
+            #config 3GB zram size with memory 6 GB
+            echo 3221225472 > /sys/block/zram0/disksize
         else
-            #config 4GB zram size with memory greater than 6GB
-            echo 4294967296 > /sys/block/zram0/disksize
+            #config 3.5GB zram size with memory greater than 6GB
+            echo 3758096384 > /sys/block/zram0/disksize
         fi
 
         # ZRAM may use more memory than it saves if SLAB_STORE_USER
