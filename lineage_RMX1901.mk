11ec99b3b8d83feed1d56ec093e19f7e2403c5d7
Ij-Israfil <israfilkhan494@gmail.com>
2025-11-19 17:01:54 +0600

    RMX1901: Bring @ProjectMatrixx
    *Vanilla build bydefault.
    * Drop offendering entries.
    
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -1,15 +1,18 @@
 #
-# Copyright (C) 2021-2024 The AOSP Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Infinity X stuff
+# Inherit some common Matrixx OS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS := true
-TARGET_SHIPS_GOOGLE_DIALER := true
-INFINITY_MAINTAINER := м∂_ιѕяαƒιℓ
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Matrixx Flags
+WITH_QS := true
+MATRIXX_CHIPSET := SDM710
+MATRIXX_BATTERY := 3765mAh
+MATRIXX_DISPLAY := 1080x2340
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -23,7 +26,7 @@ PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := infinity_RMX1901
+PRODUCT_NAME := lineage_RMX1901
 PRODUCT_MODEL := RMX1901
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
