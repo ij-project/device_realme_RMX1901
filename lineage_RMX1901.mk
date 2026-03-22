46887c8612ee9c96ef47c06c7f3440e5b67293d9
kaderbava <ksbava7325@gmail.com>
2026-03-22 19:22:47 +0000

    RMX1901: Axion-ify!
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -4,15 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Infinity X stuff
+# Inherit some common AxionOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS := true
-TARGET_SHIPS_GOOGLE_DIALER := true
-INFINITY_MAINTAINER := м∂_ιѕяαƒιℓ
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+TARGET_ENABLE_BLUR := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Axion-ify!
+AXION_CAMERA_REAR_INFO := 48,5
+AXION_CAMERA_FRONT_INFO := 16
+AXION_MAINTAINER := м∂_ιѕяαƒιℓ
+AXION_PROCESSOR := Qualcomm_Snapdragon_710_AIE
+PERF_ANIM_OVERRIDE := true
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/available_frequencies
+GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
 
 # Viper
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
@@ -23,7 +34,7 @@ PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := infinity_RMX1901
+PRODUCT_NAME := lineage_RMX1901
 PRODUCT_MODEL := RMX1901
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
