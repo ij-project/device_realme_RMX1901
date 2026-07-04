5054e8280b93961ae30abfe3dc70f89695618709
ExtremeXT <extremext@lineageos.org>
2026-07-04 13:00:29 +0600

    RMX1901: Set legacy gralloc flag
    * We don't have gralloc 4 or higher
    
    Change-Id: Ie7fb53bb16284df42fd7c3409710877d37ce58a5
    Signed-off-by: ExtremeXT <extremext@lineageos.org>
    Signed-off-by: Ij-Israfil <israfilkhan494@gmail.com>

--- device.mk
+++ device.mk
@@ -120,6 +120,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service \
     vendor.qti.hardware.display.allocator@1.0-service
 
+$(call soong_config_set_bool,libui,legacy_gralloc,true)
+
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
